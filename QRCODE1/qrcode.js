91802a06ec651943fd1929c34eaf34ab4c9f39bd
Ana Gauna <amgauna@gmail.com>
2021-12-06 21:07:12 -0300

    Update qrcode.js

--- QRCODE1/qrcode.js
+++ QRCODE1/qrcode.js
@@ -3,3 +3,8 @@ const textInput = document.getElementById("text");
 
 const qr = new QRCode(qrcode);
 qr.makeCode(textInput.value.trim());
+
+textInput.oninput = (e) => {
+    qr.makeCode(e.target.value.trim());
+};
+
